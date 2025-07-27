a082113c945b85b1f022780d739e657e78eb0ec7
Горечко Владислав <brianprott456@gmail.com>
2025-07-27 22:01:21 +0300

    ДЗ_DLL_Queue

---
+++ ДЗ_DoubleLinkedList/ДЗ_DoubleLinkedList.cpp
@@ -0,0 +1,223 @@
+﻿#include <iostream>
+using namespace std;
+
+template<typename T>
+struct Node
+{
+    T value;
+    Node* next;
+    Node* previous;
+    Node(T value, Node* next, Node* previous) :value(value), next(next), previous(previous) {}
+};
+
+template<typename T>
+class List
+{
+private:
+    Node<T>* head;
+    Node<T>* tail;
+
+public:
+    List() : head(nullptr), tail(nullptr) {}
+
+    List(const List<T>& other) : head(nullptr), tail(nullptr)
+    {
+        Node<T>* current = other.head;
+        while (current != nullptr)
+        {
+            AddToTail(current->value);
+            current = current->next;
+        }
+    }
+
+    ~List()
+    {
+        Clear();
+    }
+
+    void AddToHead(T value)
+    {
+        Node<T>* newNode = new Node<T>(value, head, nullptr);
+        if (head)
+            head->previous = newNode;
+        else
+            tail = newNode;
+        head = newNode;
+    }
+
+    void AddToTail(T value)
+    {
+        Node<T>* newNode = new Node<T>(value, nullptr, tail);
+        if (tail)
+            tail->next = newNode;
+        else
+            head = newNode;
+        tail = newNode;
+    }
+
+    void DeleteFromHead()
+    {
+        if (!head) return;
+        Node<T>* temp = head;
+        head = head->next;
+        if (head)
+            head->previous = nullptr;
+        else
+            tail = nullptr;
+        delete temp;
+    }
+
+    void DeleteFromTail()
+    {
+        if (!tail) return;
+        Node<T>* temp = tail;
+        tail = tail->previous;
+        if (tail)
+            tail->next = nullptr;
+        else
+            head = nullptr;
+        delete temp;
+    }
+
+    T getElement(int pos)
+    {
+        Node<T>* current = head;
+        int i = 0;
+        while (current != nullptr)
+        {
+            if (i == pos) return current->value;
+            current = current->next;
+            i++;
+        }
+        throw out_of_range("Invalid position");
+    }
+
+    T operator[](int pos)
+    {
+        return getElement(pos);
+    }
+
+    void Clear()
+    {
+        Node<T>* current = head;
+        while (current)
+        {
+            Node<T>* next = current->next;
+            delete current;
+            current = next;
+        }
+        head = tail = nullptr;
+    }
+
+    void Print() const
+    {
+        Node<T>* current = head;
+        while (current)
+        {
+            current->value.Print();
+            cout << " ";
+            current = current->next;
+        }
+        cout << endl;
+    }
+
+    void PrintReverse() const
+    {
+        Node<T>* current = tail;
+        while (current)
+        {
+            current->value.Print();
+            cout << " ";
+            current = current->previous;
+        }
+        cout << endl;
+    }
+
+    int Size() const
+    {
+        int count = 0;
+        Node<T>* current = head;
+        while (current)
+        {
+            count++;
+            current = current->next;
+        }
+        return count;
+    }
+};
+
+struct Vagon {
+    string type;
+	int number;
+    int capacity;
+    void Print() const
+    {
+        cout << "Тип вагону: " << type << ", Місткість: " << capacity << endl;
+    }
+};
+
+class Train
+{
+private:
+    string model;
+    int countVagons;
+    List<Vagon> vagons;
+
+public:
+    Train(string model, int countVagons) : model(model), countVagons(countVagons) {}
+    void AddVagonHead(const Vagon& vagon)
+    {
+        vagons.AddToHead(vagon);
+    }
+
+    void AddVagonTail(const Vagon& vagon)
+    {
+        vagons.AddToTail(vagon);
+    }
+
+    void DelVagonHead(const Vagon& vagon)
+    {
+        vagons.DeleteFromHead();
+    }
+
+    void DelVagonTail(const Vagon& vagon)
+    {
+        vagons.DeleteFromTail();
+    }
+   
+    void Show() const
+    {
+        cout << "Модель: " << model << ", К-кість вагонів: " << countVagons << endl;
+        vagons.Print();
+    }
+    int GetCountVagons() const
+    {
+        return countVagons;
+	}
+
+};
+
+
+int main()
+{
+    setlocale(LC_ALL, "ukrainian");
+
+    Train train("Express", 5);
+    train.AddVagonHead({ "Passenger1", 1, 100 });
+    train.AddVagonTail({ "Freight", 2, 200 });
+    train.AddVagonHead({ "Luxury1", 3, 50 });
+    train.AddVagonHead({ "Passenger2", 1, 100 });
+    train.AddVagonTail({ "Luxury2", 3, 50 });
+	train.Show(); //Passenger2, Luxury1, Passenger1, Freight, Luxury2
+
+	train.DelVagonHead({ "Passenger2", 1, 100 });
+	train.Show(); //Luxury1, Passenger1, Freight, Luxury2
+
+    cout << "Загальна кількість вагонів: " << train.GetCountVagons() << endl;
+
+    Train copiedTrain = train;
+    copiedTrain.Show();
+
+
+	return 0;
+}
---
+++ ДЗ_Queue/ДЗ_Queue.cpp
@@ -0,0 +1,167 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+template<typename T>
+class PriorityQueue
+{
+private:
+    struct Element
+    {
+        T value;
+        int priority;
+    };
+
+    Element* arr;
+    int size;
+
+public:
+    PriorityQueue() : arr(nullptr), size(0) {}
+
+    ~PriorityQueue()
+    {
+        delete[] arr;
+    }
+
+    void Enqueue(T value, int priority)
+    {
+        int index = 0;
+        while (index < size && arr[index].priority >= priority)
+            index++;
+
+        Element* temp = new Element[size + 1];
+
+        for (int i = 0; i < index; i++)
+            temp[i] = arr[i];
+
+        temp[index] = { value, priority };
+
+        for (int i = index; i < size; i++)
+            temp[i + 1] = arr[i];
+
+        delete[] arr;
+        arr = temp;
+        size++;
+    }
+
+    T Dequeue()
+    {
+        if (IsEmpty()) throw out_of_range("Черга порожня");
+
+        T value = arr[0].value;
+        for (int i = 0; i < size - 1; i++)
+            arr[i] = arr[i + 1];
+        size--;
+        return value;
+    }
+
+    bool IsEmpty() const
+    {
+        return size == 0;
+    }
+
+    void Print() const
+    {
+        if (IsEmpty())
+        {
+            cout << "[Черга порожня]" << endl;
+            return;
+        }
+
+        cout << "Черга друку:\n";
+        for (int i = 0; i < size; i++)
+        {
+            cout << "- " << arr[i].value.filename
+                << " (" << arr[i].value.position
+                << "), пріоритет: " << arr[i].priority << endl;
+        }
+    }
+};
+struct Document
+{
+    string filename;
+    string position;
+
+    void Print() const
+    {
+        cout << "Файл: " << filename << " | Посада: " << position << endl;
+    }
+};
+
+int getPriorityByPosition(const string& position)
+{
+    if (position == "гість") return 1;
+    if (position == "адміністратор") return 2;
+    if (position == "менеджер") return 3;
+    if (position == "директор") return 4;
+    return 0;
+}
+
+int main()
+{   
+{
+    setlocale(LC_ALL, "ukrainian");
+
+    PriorityQueue<Document> printerQueue;
+    int choice;
+
+    while (true)
+    {
+        cout << "\n===== Меню принтера =====\n";
+        cout << "1. Додати документ у чергу\n";
+        cout << "2. Виконати друк\n";
+        cout << "3. Переглянути чергу\n";
+        cout << "0. Вихід\n";
+        cout << "Ваш вибір: ";
+        cin >> choice;
+        cin.ignore();
+
+        if (choice == 1)
+        {
+            Document doc;
+            cout << "Введіть ім'я файлу: ";
+            getline(cin, doc.filename);
+            cout << "Введіть посаду (гість, адміністратор, менеджер, директор): ";
+            getline(cin, doc.position);
+            int pr = getPriorityByPosition(doc.position);
+
+            if (pr == 0)
+            {
+                cout << "Невідома посада. Документ не додано.\n";
+            }
+            else
+            {
+                printerQueue.Enqueue(doc, pr);
+                cout << "Документ успішно додано.\n";
+            }
+        }
+        else if (choice == 2)
+        {
+            if (printerQueue.IsEmpty())
+            {
+                cout << "Черга порожня. Немає що друкувати.\n";
+            }
+            else
+            {
+                Document doc = printerQueue.Dequeue();
+                cout << "=== Друкується документ ===\n";
+                doc.Print();
+            }
+        }
+        else if (choice == 3)
+        {
+            printerQueue.Print();
+        }
+        else if (choice == 0)
+        {
+            cout << "Завершення програми...\n";
+            break;
+        }
+        else
+        {
+            cout << "Невірний вибір. Спробуйте ще раз.\n";
+        }
+    }
+
+    return 0;
+}
