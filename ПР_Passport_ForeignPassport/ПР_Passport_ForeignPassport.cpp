b6463838c7c8289b37b289a22808ab0ee9b70c65
Горечко Владислав <brianprott456@gmail.com>
2025-08-16 10:55:25 +0300

    ПР Паспорт Аспірант

---
+++ ПР_Passport_ForeignPassport/ПР_Passport_ForeignPassport.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+class Passport {
+	string name;
+	string surname;
+	int age;
+	string patronymic;
+	string passportNumber;
+	string citizenship;
+public:
+	Passport(string n, string s, int a, string p, string num, string c)
+		: name(n), surname(s), age(a), patronymic(p), passportNumber(num), citizenship(c) {
+	}
+	void Print() const {
+		cout << "Ім'я: " << name << "\nПрізвище: " << surname << "\nВік: " << age << "\nПо батькові: " << patronymic << "\nНомер паспорта: " << passportNumber << "\nГромадянство: " << citizenship << endl;
+	}
+};
+
+class ForeignPassport : public Passport {
+	string visa;
+	string foreignpassportNumber;
+public:
+	ForeignPassport(string n, string s, int a, string p, string num, string c, string v, string fpn)
+		: Passport(n, s, a, p, num, c), visa(v), foreignpassportNumber(fpn){
+	}
+	void Print() const {
+		Passport::Print();
+		cout << "Віза: " << visa << "\nНомер закордонного паспорта: " << foreignpassportNumber << endl;
+	}
+};
+
+int main()
+{
+	setlocale(LC_ALL, "ukrainian");
+
+	Passport passport("Влад", "Мельник", 30, "Микитович", "AB1234567", "Україна");
+	ForeignPassport foreignPassport("Петро", "Петров", 28, "Петрович", "XY9876543", "Україна", "Шенген", "AB1234567");
+
+	cout << "Паспорт:\n";
+	passport.Print();
+	cout << endl;
+	cout << "Закордонний паспорт:\n";
+	foreignPassport.Print();
+}
---
+++ ПР_StudentAspirant/ПР_StudentAspirant.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+class Student {
+    public:
+    string name;
+    string surname;
+    int age;
+    void Print() const {
+        cout << "Ім'я: " << name << "\nПрізвище: " << surname << "\nВік: " << age << endl;
+    }
+};
+
+class Aspirant : public Student {
+    public:
+    string thesisTopic;
+    void Print() const {
+        Student::Print();
+        cout << "\nТема дисертації: " << thesisTopic << endl;
+    }
+};
+
+int main()
+{
+    setlocale(LC_ALL, "ukrainian");
+
+	Student student1;
+	Aspirant aspirant1;
+
+	student1.name = "Омелько";
+    student1.surname = "Кайдаш";
+    student1.age = 19;
+
+	aspirant1.name = "Лаврін";
+    aspirant1.surname = "Кайдаш";
+    aspirant1.age = 21;
+	aspirant1.thesisTopic = "Вивчення впливу кліматичних змін на сільське господарство";
+
+	cout << "Студент:\n";
+    student1.Print();
+	cout << endl;
+    cout << "Аспірант:\n";
+    aspirant1.Print();
+}
+
