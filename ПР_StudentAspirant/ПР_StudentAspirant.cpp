0fa4aff204c66a0e12376567a7f0496f573f3757
Горечко Владислав <brianprott456@gmail.com>
2025-08-16 11:35:07 +0300

    Update ПР_StudentAspirant.cpp

--- ПР_StudentAspirant/ПР_StudentAspirant.cpp
+++ ПР_StudentAspirant/ПР_StudentAspirant.cpp
@@ -3,21 +3,31 @@
 using namespace std;
 
 class Student {
-    public:
     string name;
     string surname;
     int age;
+public:
+    Student() : name(""), surname(""), age(0) {}
+    Student(string n, string s, int a) : name(n), surname(s), age(a) {}
+
     void Print() const {
         cout << "Ім'я: " << name << "\nПрізвище: " << surname << "\nВік: " << age << endl;
     }
+
+    ~Student() = default;
 };
 
 class Aspirant : public Student {
-    public:
     string thesisTopic;
-    void Print() const {
+public:
+    Aspirant() : Student(), thesisTopic("") {}
+    Aspirant(string n, string s, int a, string topic)
+        : Student(n, s, a), thesisTopic(topic) {
+    }
+
+    void Print(){
         Student::Print();
-        cout << "\nТема дисертації: " << thesisTopic << endl;
+        cout << "Тема дисертації: " << thesisTopic << endl;
     }
 };
 
@@ -25,22 +35,14 @@ int main()
 {
     setlocale(LC_ALL, "ukrainian");
 
-	Student student1;
-	Aspirant aspirant1;
+    Student student1("Омелько", "Кайдаш", 19);
+    Aspirant aspirant1("Лаврін", "Кайдаш", 21, "Вивчення впливу кліматичних змін на сільське господарство");
 
-	student1.name = "Омелько";
-    student1.surname = "Кайдаш";
-    student1.age = 19;
-
-	aspirant1.name = "Лаврін";
-    aspirant1.surname = "Кайдаш";
-    aspirant1.age = 21;
-	aspirant1.thesisTopic = "Вивчення впливу кліматичних змін на сільське господарство";
-
-	cout << "Студент:\n";
+    cout << "Студент:\n";
     student1.Print();
-	cout << endl;
-    cout << "Аспірант:\n";
-    aspirant1.Print();
-}
 
+    cout << "\nАспірант:\n";
+    aspirant1.Print();
+
+    return 0;
+}
